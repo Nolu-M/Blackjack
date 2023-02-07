efdbbab1923ccaa1465c7cbbaa80c05cfefc18e0
Nolu-M <119700411+Nolu-M@users.noreply.github.com>
2023-02-07 15:19:12 +0200

    more changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let firstCard = 3
-let secondCard = 8
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
 let cards = [firstCard, secondCard] //array - ordered list of items
 let sum = firstCard + secondCard
 let hasBlackJack = false
@@ -9,12 +9,19 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+function getRandomCard() {
+    return 5
+}
+
 function startGame() {
     renderGame()
 }
 
 function renderGame() {
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -31,7 +38,8 @@ function renderGame() {
 function newCard() {
     console.log("Drawing a new card from the deck!")
 
-    let card = 5
+
+    let card = getRandomCard()
     sum += card
     cards.push(card)
     startGame()
